cf1c58ee2cf1bf3a016f19ad1e42505f5f3b90d2
HG300404 <nglienhg000@gmail.com>
2023-12-24 16:39:21 +0700

    hhi

--- client_app/src/API/User.jsx
+++ client_app/src/API/User.jsx
@@ -1,32 +1,36 @@
-import axiosClient from './axiosClient'
+import axios from "axios";
+import axiosClient from "./axiosClient";
 
 const User = {
+  Get_All_User: () => {
+    const url = "/api/User";
+    return axiosClient.get(url);
+  },
 
-    Get_All_User: () => {
-        const url = '/api/User'
-        return axiosClient.get(url)
-    },
+  Get_User: (id) => {
+    const url = `/api/User/${id}`;
+    return axiosClient.get(url);
+  },
 
-    Get_User: (id) => {
-        const url = `/api/User/${id}`
-        return axiosClient.get(url)
-    },
+  Put_User: (data) => {
+    const url = `/api/User`;
+    return axiosClient.put(url, data);
+  },
 
-    Put_User: (data) => {
-        const url = `/api/User`
-        return axiosClient.put(url, data)
-    },
+  Login: async (data) => {
+    const url = "/api/login";
+    return axiosClient.post(url, data);
+  },
 
-    Get_Detail_User: (query) => {
-        const url = `/api/User/detail/login${query}`
-        return axiosClient.get(url)
-    },
+  Get_Detail_User: (query) => {
+    const url = `/api/login${query}`;
+    return axiosClient.get(url);
+  },
 
-    Post_User: (data) => {
-        const url = '/api/User'
-        return axiosClient.post(url, data)
-    }
+  Post_User: (data) => {
+    const url = "/api/User";
+    return axiosClient.post(url, data);
+  },
+};
 
-}
-
-export default User
+export default User;
