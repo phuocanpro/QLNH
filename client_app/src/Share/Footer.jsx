5aaa4b04646b8a4bc0b2556246ce479348a2e9af
phuocanpro <anhtp.22ite@vku.udn.vn>
2023-12-26 14:59:55 +0700

    design header, footer

--- client_app/src/Share/Footer.jsx
+++ client_app/src/Share/Footer.jsx
@@ -9,90 +9,36 @@ Footer.propTypes = {
 function Footer(props) {
     return (
         <div className="footer">
-            {/* <div className="footer-static-top">
-                <div className="container">
-                    <div className="footer-shipping pt-60 pb-55 pb-xs-25">
-                        <div className="row">
-                            <div className="col-lg-3 col-md-6 col-sm-6 pb-sm-55 pb-xs-55">
-                                <div className="li-shipping-inner-box">
-                                    <div className="shipping-icon">
-                                        <img src={Global.Footer1} alt="Shipping Icon" />
-                                    </div>
-                                    <div className="shipping-text">
-                                        <h2>Free Delivery</h2>
-                                        <p>And free returns. See checkout for delivery dates.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3 col-md-6 col-sm-6 pb-sm-55 pb-xs-55">
-                                <div className="li-shipping-inner-box">
-                                    <div className="shipping-icon">
-                                        <img src={Global.Footer2} alt="Shipping Icon" />
-                                    </div>
-                                    <div className="shipping-text">
-                                        <h2>Safe Payment</h2>
-                                        <p>Pay with the world's most popular and secure payment methods.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3 col-md-6 col-sm-6 pb-xs-30">
-                                <div className="li-shipping-inner-box">
-                                    <div className="shipping-icon">
-                                        <img src={Global.Footer3} alt="Shipping Icon" />
-                                    </div>
-                                    <div className="shipping-text">
-                                        <h2>Shop with Confidence</h2>
-                                        <p>Our Buyer Protection covers your purchasefrom click to delivery.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3 col-md-6 col-sm-6 pb-xs-30">
-                                <div className="li-shipping-inner-box">
-                                    <div className="shipping-icon">
-                                        <img src={Global.Footer4} alt="Shipping Icon" />
-                                    </div>
-                                    <div className="shipping-text">
-                                        <h2>24/7 Help Center</h2>
-                                        <p>Have a question? Call a Specialist or chat online.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
+           
             <div className="footer-static-middle">
                 <div className="container">
                     <div className="footer-logo-wrap pt-50 pb-35">
                         <div className="row">
                             <div className="col-lg-4 col-md-6">
                                 <div className="footer-logo">
-                                    <img src={Global.Logo} style={{ width: '20rem'}} alt="Footer Logo" />
-                                    <p className="info">
-                                        We are a team of designers and developers that create high quality HTML Template & Woocommerce, Shopify Theme.
-                                </p>
+                                    <img src={Global.Logo} style={{ width: '200px'}} alt="Footer Logo" />
                                 </div>
                                 <ul className="des">
                                     <li>
                                         <span>Address: </span>
-                                    828 Sư Vạn Hạnh, Quận 10, TP.HCM
+                                    426 Trần Đại Nghĩa, Hòa Quý, Ngũ Hành Sơn, Đà Nẵng
                                 </li>
                                     <li>
                                         <span>Phone: </span>
-                                        <a href="#">0763557366</a>
+                                        <a href="#">0826519466</a>
                                     </li>
                                     <li>
                                         <span>Email: </span>
-                                        <a href="mailto://tienkim9920@gmail.com">tienkim9920@gmail.com</a>
+                                        <a href="mailto://nglien000@gmail.com">nglien000@gmail.com.com</a>
                                     </li>
                                 </ul>
                             </div>
                                 <div className="col-lg-2 col-md-3 col-sm-6">
                                     <div className="footer-block">
-                                        <h3 className="footer-block-title">Product</h3>
+                                        <h3 className="footer-block-title">Dishes</h3>
                                         <ul>
                                             <li><a href="#">Prices drop</a></li>
-                                            <li><a href="#">New products</a></li>
+                                            <li><a href="#">New dishes</a></li>
                                             <li><a href="#">Best sales</a></li>
                                             <li><a href="#">Contact us</a></li>
                                         </ul>
